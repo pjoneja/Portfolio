cd80ef4a7c432ae5773d80a0856eb6a9fdcdfa07
Pranav Joneja <joneja@cooper.edu>
2015-07-19 16:08:54 -0400

    new

---
+++ Timeline of Jet Aviation/Timeline-Jet-Aviation.ino
@@ -0,0 +1,25 @@
+//buttons for 0-9 and "back"
+int button[11] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+int prev_button[11] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+void setup() {
+  for (int i = 2; i < 13; i++) {
+    pinMode(i, INPUT);
+  }
+  Keyboard.begin();
+}
+
+void loop() {
+  for (int n = 0; n < 11; n++) {
+    button[n] = digitalRead(n + 2);
+    if ((button[n] != prev_button[n]) && (button[n] == HIGH)) { //debounce
+      if (n == 10) {
+        Keyboard.print("b"); //for "back" button, print "b"
+      } else {
+        Keyboard.print(n); //for 0-9 buttons, print 0-9 accordingly
+      }
+    }
+    prev_button[n] = button[n];
+  }
+}
+
