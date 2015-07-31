06fe7fe95163412e520d160c360f993a2d5cec08
Pranav Joneja <pranavjoneja96@gmail.com>
2015-07-31 10:30:51 -0400

    Update pixelator.py

--- Pixelator/pixelator.py
+++ Pixelator/pixelator.py
@@ -8,21 +8,21 @@ from PIL import Image
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.IN)
 GPIO.setup(17, GPIO.IN)
-GPIO.setup(9, GPIO.IN)
+GPIO.setup(9, GPIO.IN) #quit button
 
 pixelsize = 0
 prev_plus = 0
 prev_minus = 0
 
-h1 = 26
-v1 = 18
-h = [h1,2*h1,3*h1,6*h1,12*h1,48*h1]
+h1 = 26 #lowest horizontal and vertical resolutions
+v1 = 19 
+h = [h1,2*h1,3*h1,6*h1,12*h1,48*h1] #resolution increases exponentially
 v = [v1,2*v1,3*v1,6*v1,12*v1,48*v1]
 
 camera = picamera.PiCamera()
 camera.vflip = True
-camera.image_effect = 'denoise'
-camera.led = False
+camera.image_effect = 'denoise' #sharper image
+camera.led = False #no red LED on camera face to hide the camera in plain sight
 camera.resolution = (h[0],v[0])
 camera.start_preview()
 
@@ -30,25 +30,25 @@ try:
 	while True:
 		plus = GPIO.input(17)
 		minus = GPIO.input(4)
-		if ((not prev_plus) and plus):
+		if ((not prev_plus) and plus): #debounce
     			if (pixelsize < 5):
-				pixelsize += 1
-                                if (pixelsize == 1):
-                                        img = Image.open('black.png')
+				pixelsize += 1 
+                                if (pixelsize == 1): #picamera is unable to capture at lowest resolution
+                                        img = Image.open('black.png') #revert to showing a black screen
                                 else:
-                                        camera.capture('cap.png')
-                                        img = Image.open('cap.png')
-                                pad = Image.new('RGB', (
-                                        ((img.size[0] + 31) // 32) * 32,
+                                        camera.capture('cap.png') 
+                                        img = Image.open('cap.png') #open recently captured png in PIL 
+                                pad = Image.new('RGB', ( # create a 'canvas padding' for image to fit the overlay size using 'floor division' arithmetic
+                                        ((img.size[0] + 31) // 32) * 32, #overlay must be a multiple of 32px by 16px
                                         ((img.size[1] + 15) // 16) * 16,
                                         ))
                                 pad.paste(img, (0, 0))
                                 o = camera.add_overlay(pad.tostring(), size=img.size)
-                                o.alpha = 255
-                                o.layer = 3
-    				camera.resolution = (h[pixelsize],v[pixelsize])
+                                o.alpha = 255 #fully opaque
+                                o.layer = 3 #top layer
+    				camera.resolution = (h[pixelsize],v[pixelsize]) #change resolution AFTER camera.capture()
     				sleep(0.1)
-    				camera.remove_overlay(o)
+    				camera.remove_overlay(o) 
 		if ((not prev_minus) and minus):
 			if (pixelsize > 0):			
 				pixelsize -= 1
